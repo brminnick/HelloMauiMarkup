9eb3fe0f1054269bc99008f9415faa0e3d93ff9a
Brandon Minnick <13558917+brminnick@users.noreply.github.com>
2022-04-26 16:22:23 -0700

    Update for .NET MAUI RC 2

--- src/Pages/MainPage.cs
+++ src/Pages/MainPage.cs
@@ -1,67 +1,48 @@
 ﻿using CommunityToolkit.Maui.Markup;
-using Microsoft.Maui;
 using Microsoft.Maui.Controls;
-using Microsoft.Maui.Devices;
-using static CommunityToolkit.Maui.Markup.GridRowsColumns;
 
 namespace HelloMauiMarkup;
 
 class MainPage : BaseContentPage<MainViewModel>
 {
-	public MainPage(IDeviceInfo deviceInfo, MainViewModel mainViewModel) : base(mainViewModel)
+	public MainPage(MainViewModel mainViewModel) : base(mainViewModel)
 	{
 		Content = new ScrollView
 		{
-			Content = new Grid
+			Content = new VerticalStackLayout
 			{
-				RowSpacing = 25,
-
-				RowDefinitions = Rows.Define(
-					(Row.HelloWorld, Auto),
-					(Row.Welcome, Auto),
-					(Row.Count, Auto),
-					(Row.ClickMeButton, Auto),
-					(Row.Image, Auto)),
+				Spacing = 25,
+				Padding = 30,
 
 				Children =
 				{
 					new Label()
-						.Text("Hello World" )
-						.Row(Row.HelloWorld).Font(size: 32)
-						.CenterHorizontal().TextCenter(),
+						.Text("Hello World")
+						.Font(size: 32)
+						.CenterHorizontal(),
 
 					new Label()
 						.Text("Welcome to .NET MAUI Markup Community Toolkit Sample")
-						.Row(Row.Welcome).Font(size: 18)
-						.CenterHorizontal().TextCenter(),
+						.Font(size: 18)
+						.CenterHorizontal(),
 
-					new HorizontalStackLayout
-					{
-						new Label()
-							.Text("Current Count: ")
-							.Font(bold: true)
-							.FillHorizontal().TextEnd(),
-
-						new Label()
-							.Font(bold: true)
-							.FillHorizontal().TextStart()
-							.Bind<Label, int, string>(Label.TextProperty, nameof(BindingContext.ClickCount), convert: count => count.ToString())
-
-					}.Row(Row.Count).CenterHorizontal(),
-
-					new Button { Text = "Click Me" }
-						.Row(Row.ClickMeButton)
-						.Font(bold: true)
+					new Label()
+						.Font(size: 18, bold: true)
 						.CenterHorizontal()
-						.BindCommand(nameof(BindingContext.IncrementClickMeButtonCommand)),
+						.Bind<Label, int, string>(Label.TextProperty, nameof(BindingContext.ClickCount), convert: count => $"Current Count: {count}"),
 
-					new Image { Source = "dotnet_bot.png", WidthRequest = 250, HeightRequest = 310 }
-						.Row(Row.Image)
+					new Button()
+						.Text("Click Me")
+						.Font(bold: true)
+						.BindCommand(nameof(BindingContext.IncrementClickMeButtonCommand))
+						.CenterHorizontal(),
+
+					new Image()
+						.Source("dotnet_bot.png")
+						.Size(250, 310)
 						.CenterHorizontal()
 				}
-			}.Padding(deviceInfo.Platform == DevicePlatform.iOS ? new Thickness(30, 60, 30, 30) : new Thickness(30))
+			}
 		};
 	}
-
-	enum Row { HelloWorld, Welcome, Count, ClickMeButton, Image }
 }
